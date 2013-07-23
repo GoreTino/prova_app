0201f37ead217f14af42846b876d14651fff6090
Jwa, Min-Kyung <ehjwa@chol.com>
2013-07-23 10:38:06 +0900

    1st Commit

---
+++ application/views/head.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<!-- Bootstrap -->
+			<link href="/static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+			<style>
+				body{
+					padding-top:60px;
+				}
+				.form_control{
+					padding-top:20px;
+				}
+			</style>		
+			<link href="/static/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">		    
+        </head>
+		<body>
+			<div class="navbar navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container">
+
+						<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+
+						<!-- Be sure to leave the brand out there if you want it shown -->
+					    <a class="brand" href="#">JavaScript</a>
+					 
+					    <!-- Everything you want hidden at 940px or less, place within here -->
+					    	<div class="nav-collapse collapse">
+					        	<ul class="nav pull-right">
+					        		<?php
+					        		if($this->session->userdata('is_login')){
+					        		?>
+						        		<li><a href="/index.php/auth/logout">Log Out</a></li>
+					        		<?php
+					        		} else {
+					        		?>
+						        		<li><a href="/index.php/auth/login">Log In</a></li>
+						        		<li><a href="/index.php/auth/register">Register</a></li>
+						        	<?php
+						        	}
+						        	?>
+						        </ul>				        
+					    	</div>
+					    </div>
+
+					</div>
+				</div>
+			</div>
+		</body>
